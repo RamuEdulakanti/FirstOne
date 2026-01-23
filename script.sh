0120102e7a03c42e736e91209c07048c4210cfb9
Ubuntu <ubuntu@ip-172-31-10-41.ap-south-1.compute.internal>
2026-01-23 10:17:14 +0000

    first

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+{
+set -x
+# list of s3 buckets
+echo "Print list of s3 buckets"
+aws s3 ls
+
+#list ec2 instances
+echo "Print list of ec2 instances"
+aws ec2 describe-instances | jq '.Reservations[].Instances[] | {ID: .InstanceId, Name: (.Tags[]? | select(.Key=="Name") | .Value), status: .State.Name}' 
+
+#list of IAM users
+echo "Print list of IAM users"
+aws iam list-users >> output.json
+} >> output.json
+
+
